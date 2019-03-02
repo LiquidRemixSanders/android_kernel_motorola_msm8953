f0ae87c85a33c944a024d2359e305768c0d5290f
mendigodepijama <mendigodepijama@gmail.com>
2019-03-02 13:28:51 -0300

    arch/drives: add Blu_Schedutil
    Signed-off-by: Jadilson Guedes <jadilson12@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -563,6 +563,11 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
 extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartmax_eps)
+#elif CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL
+extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
+extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
